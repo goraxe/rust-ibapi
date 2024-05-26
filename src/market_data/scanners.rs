f501adeb849cb713907089b34118b591d726a428
goraxe <goraxe@gmail.com>
2024-05-25 17:05:56 -0700

    fix(scanners): add default initializer for ScannerSubscription

--- src/market_data/scanners.rs
+++ src/market_data/scanners.rs
@@ -37,6 +37,34 @@ pub struct ScannerSubscription {
     stock_type_filter: String
 }
 
+impl Default for ScannerSubscription {
+    fn default() -> Self {
+        Self {
+            number_of_rows: -1,
+            instrument: "".to_string(),
+            location_code: "".to_string(),
+            scan_code: "".to_string(),
+            above_price:  f64::MAX,
+            below_price: f64::MAX,
+            above_volume: i32::MAX,
+            market_cap_above: f64::MAX,
+            market_cap_below: f64::MAX,
+            moody_rating_above: "".to_string(),
+            moody_rating_below: "".to_string(),
+            sp_rating_above: "".to_string(),
+            sp_rating_below: "".to_string(),
+            maturity_date_above: "".to_string(),
+            maturity_date_below: "".to_string(),
+            coupon_rate_above: f64::MAX,
+            coupon_rate_below: f64::MAX,
+            exclude_converiable: i32::MAX,
+            average_options_volume_above: i32::MAX,
+            scanner_settings_pairs: "".to_string(),
+            stock_type_filter: "".to_string()
+        }
+    }
+}
+
 pub struct ScannerData {
 
     contract: ContractDetails,
