b2de1b074b434dfd4d1b1981742430c59eeefa74
goraxe <goraxe@gmail.com>
2024-06-12 14:41:39 -0700

    debug(scanners): add trace log levels

--- src/market_data/scanners/decoders.rs
+++ src/market_data/scanners/decoders.rs
@@ -1,3 +1,5 @@
+use log::trace;
+
 use crate::{contracts::{self, Contract, ContractDetails}, messages::ResponseMessage, Error};
 
 use super::ScannerData;
@@ -9,26 +11,43 @@ pub(crate) fn decode_scan_data_list(message: &mut ResponseMessage ) -> Result<Ve
     message.skip(); // version
     message.skip(); // request id 
 
+    trace!("parsing num_items");
     let num_items = message.next_int()?;
 
     let mut results = Vec::<ScannerData>::new();
 
     for _ in 0..num_items {
+        trace!("parsing rank");
         let rank = message.next_int()?;
+        trace!("parsing contract");
         let contract_id = message.next_long()?;
+        trace!("parsing symbol");
         let contract_contract_symbol = message.next_string()?;
+        trace!("parsing sec type");
         let contract_contract_sectype = message.next_string()?;
+        trace!("parsing last trade");
         let contract_contract_last_trade = message.next_string()?;
+        trace!("parsing contract strike");
         let contract_contract_strike = message.next_double()?;
+        trace!("parsing contract right");
         let contract_contract_right = message.next_string()?;
+        trace!("parsing contrat exchange");
         let contract_contract_exchange = message.next_string()?;
+        trace!("parsing contract currency");
         let contract_contract_currency = message.next_string()?;
+        trace!("parsing contract symbol");
         let contract_contract_local_symbol = message.next_string()?;
+        trace!("parsing contract market");
         let contract_market_name = message.next_string()?;
+        trace!("parsing contract trading class");
         let contract_contract_trading_class = message.next_string()?;
+        trace!("parsing distance");
         let distance = message.next_string()?;
+        trace!("parsing benchmark");
         let benchmark = message.next_string()?;
+        trace!("parsing projection");
         let projection = message.next_string()?;
+        trace!("parsing legs");
         let legs = message.next_string()?;
 
         let data = ScannerData{
